7ede05b2d91d410da63eedb0028bf798c7a460a8
Andre Baltieri <andrebaltieri@hotmail.com>
2021-07-23 14:39:02 -0300

    First version

---
+++ MessageClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Teams.WebHooks
+{
+    public static class MessageClient
+    {
+        public static async Task<bool> SendAsync(string url, Message card)
+        {
+            var json = JsonSerializer.Serialize(card);
+            var client = new HttpClient();
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await client.PostAsync(url, content);
+            return result.IsSuccessStatusCode;
+        }
+    }
+}
